1447a85f5b970b3a079818fab2271ac79e670748
Rodolfo Ebajan Jr. <rodolfojrxgt@gmail.com>
2025-04-05 13:36:59 +0800

    Done with practical exam

--- 03_url.js
+++ 03_url.js
@@ -1,6 +1,7 @@
+const url = require("url");
 const myUrl =
   "https://www.example.com:8080/products?category=electronics&sort=price";
-const parsedUrl = require("url").parse(myUrl, true);
+const parsedUrl = url.parse(myUrl, true);
 console.log(parsedUrl);
 console.log(parsedUrl.protocol);
 console.log(parsedUrl.hostname);
